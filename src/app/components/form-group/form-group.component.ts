a14d2de30a841d6c5abd3f6705845de8c5d388b7
blake-wright <bkw972@gmail.com>
2024-05-18 00:50:09 -0500

    Add: form validators, primeng

--- src/app/components/form-group/form-group.component.ts
+++ src/app/components/form-group/form-group.component.ts
@@ -1,31 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-form-group',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CalendarModule, ReactiveFormsModule, CommonModule],
   templateUrl: './form-group.component.html',
   styleUrl: './form-group.component.css'
 })
 export class FormGroupComponent {
 
-  person = { firstName: "Mushu", lastName: "Wright"}
+  person = { firstName: "Mushu", lastName: "Wright", birthday: undefined}
 
   profileForm = new FormGroup({
     firstName: new FormControl(this.person.firstName, [
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(2)
     ]),
     lastName: new FormControl(this.person.lastName),
+    birthday: new FormControl(this.person.birthday, [Validators.required, this.forbiddenFutureDate()])
   });
 
-  ngOnInit() {
-    this.profileForm.get('firstName')?.setValue("Mamasita");
+  get firstName() {
+    return this.profileForm.get('firstName');
   }
+
+  get birthday() {
+    return this.profileForm.get('birthday');
+  }
+
 
   onSubmit(): void {
     console.warn(this.profileForm.value);
   }
+    
+  forbiddenFutureDate(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+        // Check if control.value is a Date object
+        if (!(control.value instanceof Date)) {
+            return { 'invalidDate': { value: control.value } };
+        }
 
+        let currentDate = new Date();
+
+        // Check if control.value is in the future
+        if (control.value > currentDate) {
+            return { 'forbiddenFutureDate': { value: control.value } };
+        }
+
+        // If date is not in the future, return null
+        return null;
+    };
+}
 }
